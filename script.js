97b9cc446982b8347f8ac4641136fc0e35a73baf
Jose Szycho <joseszychowski@gmail.com>
2023-01-25 17:27:43 -0300

    Complete merge sort algorithm

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function mergeSort(array){
-    console.log(array)
     if(array.length > 1){ //if array length is 2 or more
         //splits array in two parts
         let arrayMiddlePosition = Math.trunc(array.length / 2);
@@ -13,11 +12,28 @@ function mergeSort(array){
 }
 
 function merge(arrayLeftMiddle, arrayRightMiddle){
-    return arrayLeftMiddle.concat(arrayRightMiddle);
+    let result = []; //place for storage merged array
+
+    while(arrayLeftMiddle.length != 0 && arrayRightMiddle.length != 0){ //while both of the arrays exists
+        let value;
+        if(arrayLeftMiddle[0] < arrayRightMiddle[0]){
+            value = arrayLeftMiddle.shift() //ads minor value to result and removes it from array
+        }
+        else{
+            value = arrayRightMiddle.shift(); //ads minor value to result and removes it from array
+        }
+        result.push(value);
+    }
+
+    //in case one array have a bigger value, this array will have reamaing numbers sorted, so we concat with result 
+    if(arrayLeftMiddle.length > 0) result = result.concat(arrayLeftMiddle);
+    if(arrayRightMiddle.length > 0) result = result.concat(arrayRightMiddle);
+
+    return result;
 }
 
 
-let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let testArray = [2,453, -13, 5, 3, -9, 6, 3, 53];
 
 let result = mergeSort(testArray);
 console.log(result)
